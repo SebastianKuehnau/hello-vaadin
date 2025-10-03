a5c37c55b99bcb432329469e6ac45b7ac6a996b1
Sebastian Kühnau <seb@vaadin.com>
2025-10-03 11:07:54 +0200

    **Introduce grid for displaying Person data in HelloVaadinView**

--- src/main/java/org/example/demo/ui/HelloVaadinView.java
+++ src/main/java/org/example/demo/ui/HelloVaadinView.java
@@ -1,13 +1,22 @@
 package org.example.demo.ui;
 
-import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.router.Route;
+import org.example.demo.data.Person;
+import org.example.demo.data.PersonRepository;
 
 @Route("")
 public class HelloVaadinView extends HorizontalLayout {
 
-    public HelloVaadinView() {
-        add(new H1("Hello World"));
+    public HelloVaadinView(PersonRepository repository) {
+        // configure Grid
+        var grid = new Grid<>(Person.class);
+        grid.setColumns("id", "name", "age", "email");
+        grid.setItems(repository.findAll());
+
+        setPadding(true);
+
+        add(grid);
     }
 }
